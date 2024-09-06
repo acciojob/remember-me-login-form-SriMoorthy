466ef187f9d2af52e233e4fa728f7c9249baa77a
SriMoorthy <srimoorthy27@gmail.com>
2024-09-06 13:48:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let name = document.getElementById('username').value
+let p_word = document.getElementById('password').value
+let check = document.getElementById('checkbox')
+
+function userDetails() {
+	if(check){
+		localStorage.setItem("username",name)
+		localStorage.setItem("password",p_word)
+	}
+	else{
+		localStorage.clear();
+		alert('Logged in as.')
+	}
+}
